d353aead7911e67f5f530461c8d96296c623d3a5
averywald <averywald.dev@gmail.com>
2020-11-30 13:07:42 -0600

    Initial Commit

---
+++ Travelogue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travelogue
+//
+//  Created by Avery Wald on 11/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
